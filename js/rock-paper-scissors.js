1fbbe6f0de65974d86751ef82355d4938ec14559
Chikane Mashaba <playtunes100@gmail.com>
2023-10-10 14:41:06 +0200

    Add get computer choice method that returns a choice

---
+++ js/rock-paper-scissors.js
@@ -0,0 +1,19 @@
+
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+    if(choice === 0){
+        return "rock";
+    }
+    else if(choice === 1){
+        return "paper";
+    }
+    else if(choice === 2){
+        return "scissors";
+    }
+    else{
+        return null;
+    }
+    
+}
+
