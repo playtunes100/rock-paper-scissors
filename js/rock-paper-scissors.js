958d60bd95fd495bd306bfaf153c3fba07aad1a2
Chikane Mashaba <playtunes100@gmail.com>
2023-10-10 21:14:00 +0200

    Add playRound function which plays one round of the game and returns results

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -17,3 +17,23 @@ function getComputerChoice(){
     
 }
 
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection){
+        return "It's a draw!"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! rock beats scissors"
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! scissors beats paper"
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! paper beats rock"
+    }
+    else{
+        return "You lose! "+computerSelection+" beats "+playerSelection
+    }
+}
+
