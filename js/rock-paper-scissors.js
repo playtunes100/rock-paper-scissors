fdcc1d12160910da8dfbaedfd507a344a9e4fa37
Chikane Mashaba <playtunes100@gmail.com>
2023-10-11 12:47:44 +0200

    Add semicolon for consistency and better readablility

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -21,54 +21,54 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
 
     if(playerSelection === computerSelection){
-        return "It's a draw!"
+        return "It's a draw!";
     }
     else if (playerSelection === "rock" && computerSelection === "scissors"){
-        return "You win! rock beats scissors"
+        return "You win! rock beats scissors";
     }
     else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return "You win! scissors beats paper"
+        return "You win! scissors beats paper";
     }
     else if(playerSelection === "paper" && computerSelection === "rock"){
-        return "You win! paper beats rock"
+        return "You win! paper beats rock";
     }
     else{
-        return "You lose! "+computerSelection+" beats "+playerSelection
+        return "You lose! "+computerSelection+" beats "+playerSelection;
     }
 }
 
 function game(){
-    let playerScore = 0
-    let computerScore = 0
+    let playerScore = 0;
+    let computerScore = 0;
     for (let i = 0; i<5;i++){
     
-        const playerSelection = prompt("Choose! rock, paper or scissors", "rock")
-        const computerSelection = getComputerChoice()
-        console.log("You chose "+ playerSelection+ " and computer chose "+computerSelection)
-        const roundResults = playRound(playerSelection, computerSelection)
-        console.log(roundResults)
+        const playerSelection = prompt("Choose! rock, paper or scissors", "rock");
+        const computerSelection = getComputerChoice();
+        console.log("You chose "+ playerSelection+ " and computer chose "+computerSelection);
+        const roundResults = playRound(playerSelection, computerSelection);
+        console.log(roundResults);
         if(roundResults.includes("win!")){
-            playerScore++
+            playerScore++;
         }
         else if(roundResults.includes("lose!")){
-            computerScore++
+            computerScore++;
         }
         
-        console.log("-------Score   "+playerScore+":"+computerScore+"   END ROUND-------")
+        console.log("-------Score   "+playerScore+":"+computerScore+"   END ROUND-------");
     }
     
     if(playerScore === computerScore){
-        console.log("Final Score  "+playerScore+":"+computerScore+"  It's a tie!")
+        console.log("Final Score  "+playerScore+":"+computerScore+"  It's a tie!");
     }
     else if(playerScore > computerScore){
-        console.log("Final Score  "+playerScore+":"+computerScore+"  You Win!")
+        console.log("Final Score  "+playerScore+":"+computerScore+"  You Win!");
     }
     else if(playerScore < computerScore){
-        console.log("Final Score  "+playerScore+":"+computerScore+"  you Lose!")
+        console.log("Final Score  "+playerScore+":"+computerScore+"  you Lose!");
     }
     else{
-        console.log("I don't know how you'd get here.")
+        console.log("I don't know how you'd get here.");
     }
 }
 
-game()
+game();
